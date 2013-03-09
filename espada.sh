9a201f1769e5530f9137b6a1b8e7a3ec0791da7e
Nguyễn Hà Dương <cmpitg@gmail.com>
2013-03-10 01:28:55 +0700

    Add loadpath to script

--- espada.sh
+++ espada.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-cd src
-ruby main.rb $*
+ESPADA_PATH=$(cd `dirname $0`; pwd)
+RUBYLIB="$ESPADA_PATH/src":$RUBYLIB
+
+alias espada_ruby="ruby -I '$RUBYLIB'"
+
+echo ">> Path: $ESPADA_PATH\n"
+echo ">> Ruby \$LOADPATH:"
+espada_ruby -e "puts $:"
+echo
+
+espada_ruby "$ESPADA_PATH/src/main.rb" $*
