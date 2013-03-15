0b53a7f7e7fd70c83d9e5ca17c9c473ecdfe9dc5
Nguyễn Hà Dương <cmpitg@gmail.com>
2013-03-15 15:14:51 +0700

    Extract utils -> GUI utils

--- src/espada_gui_utils.rb
+++ src/espada_gui_utils.rb
@@ -19,39 +19,37 @@
 # along with Espada.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-require 'espada_gui_utils'
-require 'espada_string_utils'
-require 'espada_datetime_utils'
-require 'espada_fs_utils'
+require 'gui/gui_constants'
 
-def eval_text(text)
-  text = text[0..-2] while text[-1] == 10 || text[-1] == 13
-  text.strip!
-
-  # Exec if the first character is `!`
-  return [`#{text.but_first}`, :shell] if text.first == "!"[0]
-
-  command_type = :ruby
-  # TODO: ugly code
-  result = begin
-    text.split("\u07ed").each do |line|
-      # The scope of `eval` is always global
-      eval(line.chomp, TOPLEVEL_BINDING) if line
-    end
-  rescue Exception => e
-    puts e
-    command_type = :shell
-    begin
-      `#{text}` if text != ""
-    rescue Exception => e
-      # Fail silently
-      ""
-    end
+def mouse_event_to_sym(event)
+  case event.button
+  when Mouse[:LeftButton]
+    :LeftButton
+  when Mouse[:RightButton]
+    :RightButton
+  when Mouse[:MiddleButton]
+    :MiddleButton
   end
-
-  [result, command_type]
 end
 
-def message(text)
-  puts ">> #{text}"
+def current_buffer
+  if App && App.current_buffer then App.current_buffer else nil end
+end
+
+def shell_buffer
+  if App && App.shell_buffer then App.shell_buffer else nil end
+end
+
+def main_window
+  if defined? App && App.main_win then App.main_win else nil end
+end
+
+def main_menubar
+  main_window.menubar if main_window
+end
+
+# Strictly speaking, Menu is different from MenuBar, but this MenuBar is
+# global, so this should be accepted
+def main_menu
+  main_menubar
 end
