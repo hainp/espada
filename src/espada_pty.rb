a0b68c97e9f70656a7e0ab14ede2b687c2a60beb
Nguyễn Hà Dương <cmpitg@gmail.com>
2013-03-16 10:46:41 +0700

    More functionality for PTY

---
+++ src/espada_pty.rb
@@ -0,0 +1,34 @@
+# encoding: UTF-8
+
+#
+# This file is a part of Espada project.
+#
+# Copyright (C) 2013 Nguyễn Hà Dương <cmpitgATgmaildotcom>
+#
+# Espada is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Espada is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Espada.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'pseudo-terminal'
+
+class EpsadaPTY < PseudoTerminal
+  def clear_output
+    @b.lines.clear = []
+  end
+
+  def get_output
+    output = @b.lines[-1]
+    @b.lines.clear
+    output
+  end
+end
