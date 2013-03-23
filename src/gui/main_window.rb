b71e78057891718dc62b7b5b654d486a3fb8a643
Nguyễn Hà Dương <cmpitg@gmail.com>
2013-03-23 15:18:08 +0700

    Add function to check if only modifiers are pressed @ main window

--- src/gui/main_window.rb
+++ src/gui/main_window.rb
@@ -110,6 +110,11 @@ class MainWindow < Qt::Widget
         || event.type == Qt::Event::KeyPress)
       key = event.key.parse_key
       keymod = event.modifiers.parse_keymod
+      if binding_list.is_modifier?(key)
+        keymod << key.to_s.sub("Key_", "").to_sym
+        keymod.uniq!
+        key = nil
+      end
 
       if event.type == Qt::Event::KeyPress
         valid = true
